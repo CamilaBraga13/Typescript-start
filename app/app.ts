93b38f023064e760dcfc5c0be5652f56ea50ac24
Camila Braga <camilabraga.997@gmail.com>
2023-04-19 15:14:34 -0300

    Changing qtd to 20

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,5 @@
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(new Date(), 10, 150);
+const negociacao = new Negociacao(new Date(), 20, 150);
 console.log(negociacao.volume);
  
